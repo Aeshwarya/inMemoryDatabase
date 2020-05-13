0b3df76a0387ea7c7347d683e003292dce86b721
Abhishek Kumar <abhishek.kumar@uipath.com>
2020-05-13 17:16:34 +0530

    Added port

--- test.py
+++ test.py
@@ -7,28 +7,29 @@ server_config["SERVERS"] = cacheConfig.SERVERS
 server_config["CURRENT_SERVER"] = "server1"
 
 
-
-server_set_url = "http://0.0.0.0:2222/set"
-server_del_url = "http://0.0.0.0:2222/expire"
-server_get_url = "http://0.0.0.0:2222/get"
+base_url = "http://0.0.0.0:"
+server_set_url = "/set"
+server_del_url = "/expire"
+server_get_url = "/get"
 
 while(True):
+    port= input("Port:")
     operation = input("Operation: ")
     if operation == "SET":
         key = input("enter key :")
         value = input("enter value :")
         params = {"key": key, "value": value}
-        resp = requests.post(server_set_url, json=params)
+        resp = requests.post(base_url+port+server_set_url, json=params)
         print("response:", resp.status_code, resp.json())
     elif operation == "GET":
         key = input("enter key :")
         params = {"key": key}
-        resp = requests.post(server_get_url, json=params)
+        resp = requests.post(base_url+port+server_get_url, json=params)
         print("response:", resp.json())
     elif operation == "DELETE":
         key = input("enter key :")
         params = {"key": key}
-        resp = requests.post(server_del_url, json=params)
+        resp = requests.post(base_url+port+server_del_url, json=params)
         print("response:", resp.status_code)
     else:
         print("invalid operation")
